7ec8a103279fde67f3999214c81c8391c3b07fd7
Luu Thien Vi <luuthienvi.github@gmail.com>
2021-11-16 02:29:57 +0700

    1. Two Sum

---
+++ two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * Auctor: Luu Thien Vi
+ */
+
+const twoSum = function (nums, target) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const difference = target - nums[i];
+
+        if (map.has(difference)) 
+			return [map.get(difference), i];        
+		map.set(nums[i], i);
+    }
+
+    return [];
+};
