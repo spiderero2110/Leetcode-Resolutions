04804ec272637141d9a4b73fc3ce14b3bdd41c15
Luu Thien Vi <luuthienvi.github@gmail.com>
2021-11-16 02:34:00 +0700

    1. Two Sum

--- two-sum.js
+++ two-sum.js
@@ -8,9 +8,8 @@ const twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         const difference = target - nums[i];
 
-        if (map.has(difference)) 
-			return [map.get(difference), i];        
-		map.set(nums[i], i);
+        if (map.has(difference)) return [map.get(difference), i];
+        map.set(nums[i], i);
     }
 
     return [];
